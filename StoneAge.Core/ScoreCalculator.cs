b3e2a827e6444a4d7f0363128a6bd38f9e800fce
Benjamin M. Heimann <heimannb@gmail.com>
2022-03-15 23:38:16 -0400

    restructuring Score method to remove duplication

--- StoneAge.Core/ScoreCalculator.cs
+++ StoneAge.Core/ScoreCalculator.cs
@@ -19,20 +19,27 @@ namespace StoneAge.Core
 
         private static readonly Dictionary<CardBottom, int> _meepleBottoms = new Dictionary<CardBottom, int>
         {
-            {CardBottom.Meeple1Score, 1},
-            {CardBottom.Meeple2Score, 2},
+            { CardBottom.Meeple1Score, 1 },
+            { CardBottom.Meeple2Score, 2 },
         };
 
         private static readonly Dictionary<CardBottom, int> _toolBottoms = new Dictionary<CardBottom, int>
         {
-            {CardBottom.Tool1Score, 1},
-            {CardBottom.Tool2Score, 2},
+            { CardBottom.Tool1Score, 1 },
+            { CardBottom.Tool2Score, 2 },
         };
 
         private static readonly Dictionary<CardBottom, int> _farmBottoms = new Dictionary<CardBottom, int>
         {
-            {CardBottom.Farm1Score, 1},
-            {CardBottom.Farm2Score, 2},
+            { CardBottom.Farm1Score, 1 },
+            { CardBottom.Farm2Score, 2 },
+        };
+
+        private static readonly Dictionary<CardBottom, int> _hutBottoms = new Dictionary<CardBottom, int>
+        {
+            { CardBottom.Hut1Score, 1 },
+            { CardBottom.Hut2Score, 2 },
+            { CardBottom.Hut3Score, 3 },
         };
 
         public int Score(Player player)
@@ -50,31 +57,39 @@ namespace StoneAge.Core
                 .Count(g => g.Count() > 1);
             score += duplicateCount * duplicateCount;
 
+            var playerMeepleCount = player.MeepleCount;
             foreach (var cardBottom in _meepleBottoms.Keys)
             {
                 var meepleCardCount = player.CivilizationCards.Count(c => c.CardBottom == cardBottom);
-                score += player.MeepleCount * meepleCardCount * _meepleBottoms[cardBottom];
+                score += playerMeepleCount * meepleCardCount * _meepleBottoms[cardBottom];
             }
 
             var toolTotalCount = (player.Tool1?.Value ?? 0) + (player.Tool2?.Value ?? 0) + (player.Tool3?.Value ?? 0);
+            var scoreToAdd = 0;
             foreach (var cardBottom in _toolBottoms.Keys)
             {
                 var toolCardCount = player.CivilizationCards.Count(c => c.CardBottom == cardBottom);
-                score += toolTotalCount * toolCardCount * _toolBottoms[cardBottom];
+                scoreToAdd += toolTotalCount * toolCardCount * _toolBottoms[cardBottom];
             }
+            score += scoreToAdd;
 
+            var playerFarmLevel = player.FarmLevel;
+            scoreToAdd = 0;
             foreach (var cardBottom in _farmBottoms.Keys)
             {
                 var farmCardCount = player.CivilizationCards.Count(c => c.CardBottom == cardBottom);
-                score += player.FarmLevel * farmCardCount * _farmBottoms[cardBottom];
+                scoreToAdd += playerFarmLevel * farmCardCount * _farmBottoms[cardBottom];
             }
+            score += scoreToAdd;
 
             var hutsCount = player.Huts.Count;
+            scoreToAdd = 0;
             foreach (var cardBottom in _hutBottoms.Keys)
             {
                 var hutCardCount = player.CivilizationCards.Count(c => c.CardBottom == cardBottom);
-                score += hutsCount * hutCardCount * _hutBottoms[cardBottom];
+                scoreToAdd += hutsCount * hutCardCount * _hutBottoms[cardBottom];
             }
+            score += scoreToAdd;
 
             return score;
         }
